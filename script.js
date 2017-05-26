bdf298d8f153022687af42ff160022b0e4007b38
Andrew Stacey <loopspace@mathforge.org>
2017-05-26 23:29:32 +0100

    A simple extra time calculator for exams.

---
+++ script.js
@@ -0,0 +1,34 @@
+function init() {
+    var btn = document.getElementById('calcbtn');
+    btn.addEventListener('click',calculate,false);
+}
+
+window.addEventListener('load',init,false);
+
+function calculate() {
+    var e;
+    var lengthhr = parseInt(document.getElementById("lengthhr").value,10);
+    var lengthmin = parseInt(document.getElementById("lengthmin").value,10);
+    var length = lengthhr*60+lengthmin;
+
+    var percent = parseInt(document.getElementById("percent").value,10);
+    
+    length *= 1 + percent/100;
+    length = Math.ceil(length);
+
+    var hr = Math.floor(length/60);
+    var min = length%60;
+
+    document.getElementById("alen").innerHTML = hr + 'hr ' + min + 'min';
+    
+    e = document.getElementById("starthr");
+    var starthr = parseInt(e.options[e.selectedIndex].value,10);
+    e = document.getElementById("startmin");
+    var startmin = parseInt(e.options[e.selectedIndex].value,10);
+
+    length += startmin;
+    hr = Math.floor(length/60) + starthr;
+    min = length%60;
+
+    document.getElementById("end").innerHTML = hr + ':' + (min < 10 ? '0' : '') + min;
+}
